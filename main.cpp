5c95d6f4968fc701ec19406e932857a39bec1a49
letsgohye1 <kohyewonwon@gmail.com>
2025-09-22 09:20:19 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main() {
 	int Num = 0;
-	int Sum = 0;
+	int OneToHundreadSum = 0;
 	int MultipleSum = 0;
 	
 	for (int j = 0; j < 100; j++)
 	{
 		Num = Num + 1;
-		Sum = Sum + Num;
+		OneToHundreadSum = OneToHundreadSum + Num;
 
 		if (Num % 3 == 0) {
 			MultipleSum = MultipleSum + Num;
 		}
 	}
 
-	cout << "1부터 100까지의 합: " << Sum << endl;
+	cout << "1부터 100까지의 합: " << OneToHundreadSum << endl;
 	cout << "1부터 100까지의 3의 배수의 합: " << MultipleSum << endl;
 }
 
