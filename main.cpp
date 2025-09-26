efb3ac6bdea885715161e0d5410e7dbd8984f9c6
letsgohye1 <kohyewonwon@gmail.com>
2025-09-26 17:46:37 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,219 +1,18 @@
 ﻿#include <iostream>
-using namespace std;
-/*
-int main()
-{
-	int Num = 0;
-	int OneToHundreadSum = 0;
-	int MultipleSum = 0;
-
-	for (int j = 1; j < 100; j++)
-	{
-		Num = Num + 1;
-		OneToHundreadSum = OneToHundreadSum + Num;
-
-		if (Num % 3 == 0)
-		{
-			MultipleSum = MultipleSum + Num;
-		}
-	}
-
-	cout << "1부터 100까지의 합: " << OneToHundreadSum << endl;
-	cout << "1부터 100까지의 3의 배수의 합: " << MultipleSum << endl;
-
-	return 0;
-
-}
-*/
-
-/*
-int main()
-{
-	int Sum = 0;
-	int OneToHundreadSum = 0;
-	int MultipleSum = 0;
-
-	for (int j = 1; j < 100; j++)
-	{
-		Sum += j;
-	}
-	cout << "1부터 100까지의 합: " << Sum << endl;
-
-	Sum = 0;
-	for (int j = 3; j < 100; j+=3)
-	{
-		Sum += j;
-	}
-	cout << "1부터 100까지의 3의 배수의 합: " << Sum << endl;
-
-	return 0;
-
-}
-*/
-
-/*
-int main()
-{
-	int Number[10] = { 0, };
-	for (int j = 0; j < 10; j++) {
-		for (int i = 0; i < 10; i++)
-		{
-			Number[i] = i + 1;
-			cout << Number[i] << ' ';
-		}
-		cout << endl;
-	}
-}
-*/
-
-/*
-int main()
-{
-	//*****
-	//****
-	//***
-	//**
-	//*
-
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5-i; j++)
-		{
-			cout << '*';
-		}
-		cout << endl;
-	}
-}
-*/
-
-/*
-int main()
-{
-	//*****
-	// ****
-	//  ***
-	//   **
-	//    *
-	for (int i = 0; i < 5; i++)
-	{
-
-		for (int p = 0; p < i; p++)
-		{
-			cout << ' ';
-		}
-		for (int j = 0; j < 5 - i; j++)
-		{
-			cout << '*';
-		}
-		cout << endl;
-
-	}
-	return 0;
-}
-
-*/
-
-/*
-int main()
-{
-	//*******
-	//8*****8
-	//88***88
-	//888*888
-
-	int count = 0;
-	cin >> count;
-
-	for (int i = 1; i <= count/2+1; i++)
-	{
-
-		for (int p = 1; p < i; p++)
-		{
-			cout << '8';
-		}
-
-		for (int j = 0; j <= count - (i*2)+1; j++)
-		{
-			cout << '*';
-		}
-
-		for (int p = 1; p < i; p++)
-		{
-			cout << '8';
-		}
-
-		cout << endl;
-
-	}
-	return 0;
-}
-*/
-
-/*
-#define Count 10
-int main()
-{
-	//********** 10
-	//*88888888*
-	//*88888888*
-	//*88888888*
-	//*88888888*
-	//*88888888*
-	//*88888888*
-	//*88888888*
-	//*88888888*
-	//********** 
-	//10
-
-	char World[Count][Count] = { '0' };
-	int PlayerX = 0;
-	int PlayerY = 0;
-	char playerShape = 'p';
-
-	//사상(Mapping)
-	char Sprites[10] = { ' ', '*' };
-
-
-	//Render
-	for (int i = 0; i < Count; i++)
-	{
-		for (int j = 0; j < Count; j++)
-		{	
-			if (PlayerX == i && PlayerY == j)
-			{
-				cout << playerShape;
-
-			}
-			if (i == 0 || i == 9 || j == 0 || j == 9)
-			{
-				World[i][j] = 1;
-				cout << World[i][j];
-			}
-			else
-			{
-				World[i][j] = 0;
-				cout << World[i][j];
-			}
-		}
-		cout << endl;
-	}
-}
-*/
-
-
 #include <conio.h> //c standard
+using namespace std;
 
 
 int World[10][10] =
 {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
-	{1, 0, 0, 0, 0, 1, 0, 1, 0, 1},
-	{1, 0, 0, 0, 0, 1, 1, 1, 0, 1},
-	{1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
@@ -224,8 +23,8 @@ int PlayerX = 1;
 int PlayerY = 1;
 char PlayerShape = 'P';
 
-//»ç»ó(Mapping)
-char Sprites[10] = { ' ', '*', }; //±×¸²À¸·Î ¹Ù²ÙÀÚ.
+//(Mapping)
+char Sprites[10] = { ' ', '*', };
 
 int KeyCode;
 
